cea09a2c9d7ef53ab58622666359a1c25dec4dac
Chris Daley <chebizarro@gmail.com>
2024-09-25 12:10:54 -0700

    Removed redundant sample app

--- example-app/src/js/example.js
+++
@@ -1,6 +0,0 @@
-import { NostrSignerPlugin } from 'nostr-signer-capacitor-plugin';
-
-window.testEcho = () => {
-    const inputValue = document.getElementById("echoInput").value;
-    NostrSignerPlugin.echo({ value: inputValue })
-}
